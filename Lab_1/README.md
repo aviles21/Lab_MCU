62dffbf614d2f7d9f461db2f713bb2a34d259dce
aviles21 <aviles2122@gmail.com>
2023-01-13 23:21:55 -0600

    Mejorando comentarios en el codigo

---
+++ Lab_1/README.md
@@ -0,0 +1,7 @@
+# Lab_1
+
+Para que el archivo de simulación .sim1 funcione correctamente, es necesario aplicar el comando make para obtener el archivo lab_1.hex a partir de lab_1.c.
+
+Es necesario abrir el archivo de simulación en el programa que el profesor subió a Mediación Virtual llamado Simulide, la versión del programa corresponde a: SimulIDE-1.0.0-RC3 R1148
+
+Si la simulación no corre apropiadamente, tal vez sea necesario recargar el archivo lab_1.hex en el PIC12f685.
--- Lab_1/lab_1.c
+++ Lab_1/lab_1.c
@@ -1,6 +1,6 @@
 #include <pic14/pic12f675.h>
 
-#define BTN GP3
+#define BTN GP3 // Definir a GP3 como el boton
 
 typedef unsigned int word;
 
@@ -13,14 +13,17 @@ void delay (unsigned int tiempo);
 
 word wait = 200;
 
+// Variables para controlar los rebotes
 char BTN_pressed = 0;
 char BTN_press = 0;
 char BTN_release = 0;
 char BTN_released = 0;
 char Bouncevalue = 500;
 
+// Variable para generar el numero aleatorio
 int rand = 0;
- 
+
+// Funcion infinita que controla la ejecucion
 void main(void)
 {
 
@@ -31,7 +34,8 @@ void main(void)
     //Loop forever
     while ( 1 )
     {	
-		// Generador de numeros aleatorios
+		// Generador de numeros aleatorios entre 1 y 6,
+        // en cada iteración del programa se incrementa
 		if (rand < 6)
         {
             rand++;
@@ -42,46 +46,44 @@ void main(void)
 		// Checkear si GP3 es presionado
         if (BTN == 1)
         {
-            // Bouncing has started so increment BTN_press with 1, for each "high" bounce
+            // Maneja el numero de veces que el boton ha sido constante
             BTN_press++;
-            // "reset" BTN_release
             BTN_release = 0;
-            // If it bounces so much that BTN_press is greater than Bouncevalue
-            // then button must be pressed
+
+            // El boton es constante en ser presionado
             if (BTN_press > Bouncevalue)
             {
-                // This is initial value of BTN_pressed. 
-                // If program gets here, button must be pressed
+                // Primera vez que el boton se presiona
                 if (BTN_pressed == 0)
                 {
+                    // Se envia el valor actual del contador como numero random
 					blink_dice(rand);
 
-                    // Setting BTN_pressed to 1, ensuring that we will 
-                    // not enter this code block again
+                    // Boton ya presionado
                     BTN_pressed = 1;
                 }
-                // LEDs toggled, set BTN_pressed to 0, so we can enter 
-                // toggle code block again
+                // Habilitar el dejar de presionar el boton
                 BTN_press = 0;
 				BTN_released = 0;
             }
         }
         else
         {
-            // Increment the "low" in the bouncing
+            // Maneja el numero de veces que el boton ha sido constante
             BTN_release++;
             BTN_press = 0;
-            // If BTN_release is greater than Bouncevalue, we do not have a
-            // pressed button
+
+            // El boton es constante en ser soltado
             if (BTN_release > Bouncevalue)
             {
 				if (BTN_released == 0)
 				{
+                    // Se espera un corto periodo y los LEDs se apagan
 					delay(wait);
 					GPIO &= (0b11001000);
 					BTN_released = 1;
 				}
-
+                // Habilitar el presionar el boton
                 BTN_pressed = 0;
                 BTN_release = 0;
             }
@@ -89,6 +91,8 @@ void main(void)
 	}
 }
 
+// Se encarga de encender los LEDs apropiados según el número
+// aleatorio generado
 void blink_dice(int rand)
 {
 	switch (rand)
@@ -123,6 +127,7 @@ void blink_dice(int rand)
 	}
 }
 
+// Función simple para generar un delay
 void delay(unsigned int tiempo)
 {
 	unsigned int i;
